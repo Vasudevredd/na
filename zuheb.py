3dd484cda64ac79737054f509b2c94ba3d35ec44
Vasudevredd <119033380+Vasudevredd@users.noreply.github.com>
2022-12-23 13:09:13 +0530

    Add files via upload

---
+++ zuheb.py
@@ -0,0 +1,10 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/STUDENT/Desktop/zuheb.py =================
+ENTER a = 5
+ENTER b = 6
+ENTER c = 5
+sum is: 16
+the averge is: 5.333333333333333
+>>> 
